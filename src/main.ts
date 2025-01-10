d42912abc8baa1ce70cbbe15f39385abd671abf6
joonseokchung <joonseok99@naver.com>
2025-01-10 17:26:39 +0900

    docs/update readme

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(TodoModule, { cors: true });
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('nest-practice')
+    .setDescription('practice project')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('HIGT')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
